9bd8259abf886786ee6c44b4b9f5358f95a02866
Tanushreehurali <tanuhurali@gmail.com>
2025-11-13 17:19:23 +0530

    Create employee.py

---
+++ employee.py
@@ -0,0 +1,13 @@
+name=(intput("enter a employee name"))
+id=(input("enter a employee id"))
+age=(input("enter a employee age"))
+salary=(input("enetr a employee salary"))
+experience=(input("enetr a employee experience"))
+
+printf("empolyee details:")
+print(f"employee name:{name}")
+print(f"employee id:{id}")
+print(f"employee age:{age}")
+print(f"employee salary:{salary}")
+print(f"employee experience:{experience}")
+
